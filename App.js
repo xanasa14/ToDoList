bd3e184f8e0d8b2bed351f0a211ca8aad8ecb120
xanasa14 <xanasa14@hotmail.com>
2022-08-15 00:19:14 -0500

    etc

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ export default function App() {
             <View style={styles.goalItem} key={goal}>
             <Text style={styles.goalText}>
               {goal}
+              
             </Text>
         </View>
         ))}
