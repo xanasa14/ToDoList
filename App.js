99cba2badb8db61b96367ed507a867364006cdf3
xanasa14 <xanasa14@hotmail.com>
2022-08-19 22:26:56 -0500

    Added delete option

--- App.js
+++ App.js
@@ -15,13 +15,30 @@ export default function App() {
       {text: enteredGoalText, id: Math.random().toString()},
     ]);
   }
+
+function deleteGoalHandler(id){
+  setCourseGoals((currentCourseGoals) => {
+
+    return currentCourseGoals.filter((goal) => 
+      goal.id !== id);
+  });
+}
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
 
       <View style={styles.goalsContainer}>
-      <FlatList data={courseGoals} renderItem={(itemData) => {
-        return <GoalItem text={itemData.item.text}/>;
+      <FlatList 
+        data={courseGoals} 
+        renderItem={(itemData) => {
+        return(
+          <GoalItem 
+              text={itemData.item.text} 
+              id ={itemData.item.id}
+              onDeleteItem={deleteGoalHandler}
+            />
+        );
       }} 
       keyExtractor={(item, index) =>{
         return item.id;
