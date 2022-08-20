665a59c032ccfaa4d88d990976e6f375e650c023
xanasa14 <xanasa14@hotmail.com>
2022-08-19 22:39:12 -0500

    Added Ripple effect for Android and Iphone on press

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,16 @@ import {StyleSheet, View, Text, Pressable}  from 'react-native';
 
 function GoalItem(props) {
     return( 
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+        <Pressable 
+            //Android
+            android_ripple= {{color:'#210644'}}
+            //Iphone
+            style={({pressed}) => pressed && styles.pressedItem }
+            onPress={props.onDeleteItem.bind(this, props.id)}
+           
+            >
+
+            
             <View style={styles.goalItem}>
                 <Text style={styles.goalText}>
                     {props.text}
@@ -17,12 +26,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
-        
+      },
+      pressedItem:{
+        opacity:0.5,
       },
       goalText:{
         color: 'white',
+        padding:8
       }
 });
